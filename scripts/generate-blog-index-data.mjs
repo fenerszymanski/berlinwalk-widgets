ad16fba92a7640a99c1c58632fe880cb76427c41
fenerszymanski <yusuf.ucuz@gmail.com>
2026-06-30 08:27:53 +0200

    Refresh blog index for plug adapter post

--- scripts/generate-blog-index-data.mjs
+++ scripts/generate-blog-index-data.mjs
@@ -490,6 +490,7 @@ function relatedToolSlugFor(post) {
   if (/(doctor-in-berlin|doctor in berlin|feel unwell|medical help berlin)/.test(s)) return 'berlin-medical-help-router';
   if (/(breakfast-in-berlin|breakfast in berlin|brunch|bakery breakfast|coffee morning)/.test(s)) return 'berlin-breakfast-clock';
   if (/(german-signs-in-berlin|german signs in berlin|ausgang|ersatzverkehr|sign decoder)/.test(s)) return 'berlin-sign-decoder';
+  if (/(berlin-plug-adapter|plug adapter|travel adapter|type f|schuko|berlin sockets|charger|usb-c|power bank)/.test(s)) return 'berlin-plug-adapter-checker';
   if (/(pharmacy|apotheke|notdienst|medicine|116117)/.test(s)) return 'pharmacy-in-berlin-helper';
   if (/(alternative-transport|alternative transport|mobility app|miles|car sharing|bike sharing|scooter)/.test(s)) return 'berlin-mobility-app-picker';
   if (/(zoo-berlin-vs-tierpark|tierpark|zoo berlin vs tierpark)/.test(s)) return '';
