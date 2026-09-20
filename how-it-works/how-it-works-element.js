95d8258fa784301e2f9d40c94cdd13641d4c0641
fenerszymanski <yusuf.ucuz@gmail.com>
2026-09-20 13:21:08 +0200

    Align tour FAQs and guidance with refundable reservation deposit

--- how-it-works/how-it-works-element.js
+++ how-it-works/how-it-works-element.js
@@ -16,7 +16,7 @@ class BWHowItWorksElement extends HTMLElement {
 
   _render() {
     const steps = [
-      { num: 1, icon: 'calendar', title: 'Reserve free', subtitle: 'On Freetour, no payment needed', chip: '~30 SEC' },
+      { num: 1, icon: 'calendar', title: 'Reserve your place', subtitle: '€2 refundable deposit per guest', chip: '~30 SEC' },
       { num: 2, icon: 'umbrella', title: 'Show up at Alexanderplatz', subtitle: 'Meeting point at World Clock', chip: '11:30 AM TUE-SAT' },
       { num: 3, icon: 'walking', title: 'Walk + tip what you feel', subtitle: '12 stops, Alexanderplatz to Hackescher Markt', chip: '~2 HOURS - 12 STOPS' }
     ];
@@ -391,7 +391,7 @@ class BWHowItWorksElement extends HTMLElement {
           </div>
 
           <div class="bw-how-cta-row">
-            <a class="bw-how-cta" href="https://www.berlinwalk.com/book-berlin-walking-tour/berlin-free-walking-tour-tip-based">Book your free spot</a>
+            <a class="bw-how-cta" href="https://www.berlinwalk.com/book-berlin-walking-tour/berlin-free-walking-tour-tip-based">Reserve your place</a>
           </div>
         </div>
       </section>
